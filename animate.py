529575df3eaeef45786ae161c7b5b4a91ddc6e24
Aaron Harder <AHarder14@gmail.com>
2024-07-27 11:48:44 -0400

    Create main function. Remove inferring labels from file names. Allow for using common headers, if they already exist. Expand arguments.

--- animate.py
+++ animate.py
@@ -1,45 +1,87 @@
 import argparse
 import os
+import warnings
 
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
 parser = argparse.ArgumentParser(
-    description='Utility for making animated line graphs.')
+    description='Script for making animated line graphs from simple 2D data.')
 parser.add_argument(
-    'path',
-    help='Relative or absolute path to folder containing data.')
+    'files', nargs='+',
+    help='Names of files in directory to parse. File should share a format.')
 parser.add_argument(
-    '-t', '--title',
+    '--directory', '-d', default=None,
+    help='Relative or absolute path to directory containing data.')
+
+fig_parser = parser.add_argument_group(
+    title='Figure Arguments',
+    description='Arguments controlling figure settings.')
+fig_parser.add_argument(
+    '--title', '-t', default=None,
     help='Title of the animated figure.')
-parser.add_argument(
-    '--names', '-n', nargs='+',
-    help='Names of columns present in data files.')
+fig_parser.add_argument(
+    '--size', '-s', default=None, type=int, nargs='*',
+    help='Size of the figure to plot.')
+
+file_parser = parser.add_argument_group(
+    title='File and Plot Arguments',
+    description=(
+        'Arguments controlling individual plot behavior. '
+        'Lists should follow same order as files.'
+        ))
+file_parser.add_argument(
+    '--names', '-n', nargs='*', default=None,
+    help='Names of columns present in data and labels of x, y axises.')
+file_parser.add_argument(
+    '--inverse', '-i', action='store_true',
+    help='Flip the x and y axis for resulting plots.')
+file_parser.add_argument(
+    '--header', '-H', type=int, default=None,
+    help='Index of the row containing the table names.')
+file_parser.add_argument(
+    '--separator', '-S', default=None,
+    help='Separator to use when parsing CSV like files.')
+file_parser.add_argument(
+    '--labels', '-l', nargs='*', default=[],
+    help='Plot label for line produced for each file.')
+file_parser.add_argument(
+    '--colors', '-c', nargs='*', default=[],
+    help='Plot colors for line produced for each file.')
+file_parser.add_argument(
+    '--markers', '-m', nargs='*', default=[],
+    help='Plot markers for line produced for each file.')
 
 animations = []
 
 
-def data_to_dataframe(path: str, names: list[str]) -> pd.DataFrame:
-    data_files = [i for i in os.listdir(path) if i.endswith('dat')]
+def data_to_dataframes(
+        paths: list[str],
+        names: list[str] | None = None,
+        header: int | None = None,
+        sep: str = r'\s+') -> list[pd.DataFrame]:
     dfs = []
 
-    for i in data_files:
-        file_path = os.path.join(path, i)
-        alpha = int(
-            i
-            .split('_')[-1]
-            .split('.')[0]
-            .replace('alpha', '')
-            )
-        df = (
-            pd
-            .read_csv(
-                file_path, names=names, index_col=False,
-                header=None, sep=r'\s+')
-            .convert_dtypes()
-            )
-        dfs.append((alpha, df))
+    for i in paths:
+        match i.split('.'):
+            case _, 'csv' | 'dat':
+                df = pd.read_csv(i, index_col=False, header=header, sep=sep)
+            case _, 'xls' | 'xlsx':
+                df = pd.read_excel(i, index_col=False, header=header)
+            case _, t:
+                basename = os.path.basename(i)
+                warn = f'Skipping "{basename}": unacceptable type "{t}".'
+                warnings.warn(warn)
+                continue
+
+        if names:
+            df = df.set_axis(names, axis=1)
+        
+        dfs.append(df.convert_dtypes())
+
+    if not dfs:
+        raise ValueError('No valid file types provided.')
     
     return dfs
 
@@ -55,35 +97,92 @@ def update_line(
 
 def create_animated_plots(
         fig: plt.Figure,
+        ax: plt.Axes,
         frames: int,
         x: pd.Series,
         y: pd.Series,
         **line_args):
-    line, = ax.plot(x, y)
+    line, = ax.plot(x, y, **line_args)
     ani = animation.FuncAnimation(
         fig,update_line, frames, fargs=(x, y, line))
     animations.append(ani)
 
 
+def main(
+        files: list[str],
+        directory: str | None,
+        names: list[str],
+        header: int,
+        separator: str,
+        title: str,
+        size: str,
+        inverse: bool,
+        labels: list[str],
+        colors: list[str],
+        markers: list[str]):
+    def null_unpack(**kwargs) -> tuple:
+        return {k: v for k, v in kwargs.items() if not v is None}
+    
+    def safe_list_unpack(l: list, idx: int) -> object:
+        try:
+            return l[idx]
+        except IndexError:
+            return None
+        
+    if directory:
+        paths = [os.path.join(directory, i) for i in files]
+    else:
+        paths = files
+
+    dfs = data_to_dataframes(
+        **null_unpack(
+            **dict(
+                paths=paths, names=names, header=header, sep=separator)))
+    fig, ax = plt.subplots()
+
+    if size:
+        fig.set_size_inches(*size)
+
+    frames = max(len(df) for df in dfs)
+
+    if not names:
+        names = set.intersection(*[set(df.columns) for df in dfs])
+
+        try:
+            x_name, y_name = list(names)[:2]
+        except IndexError:
+            raise IndexError(
+                'Not enough common columns between files to select names.')
+
+    else:
+        x_name, y_name, = names
+    
+    line_args_keys = (
+        ('label', labels), ('color', colors), ('marker', markers)
+        )
+
+    if inverse:
+        x_name, y_name = y_name, x_name
+
+    for i, df in enumerate(dfs):
+        x = df[x_name]
+        y = df[y_name]
+        line_args = {
+            k: safe_list_unpack(l, i) for k, l in line_args_keys
+            }
+        create_animated_plots(
+            fig, ax, frames, x, y,
+            **null_unpack(**line_args))
+
+    if title:
+        ax.set_title(title)
+
+    ax.set_xlabel(x_name)
+    ax.set_ylabel(y_name)
+    plt.show()
+    
+
 if args := parser.parse_args():
-    data_dir = args.__dict__['path']
-    data_names = args.__dict__['names']
-    plot_title = args.__dict__['title']
-    dfs = data_to_dataframe(data_dir, data_names)
+    main(**args.__dict__)    
 else:
     quit()
-
-
-fig, ax = plt.subplots()
-frames = max(len(df) for _, df in dfs)
-x_name, y_name, = data_names
-
-for _, df in dfs:
-    create_animated_plots(fig, frames, df[x_name], df[y_name])
-
-if plot_title:
-    ax.set_title(args.__dict__['title'])
-
-ax.set_xlabel(x_name)
-ax.set_ylabel(y_name)
-plt.show()
